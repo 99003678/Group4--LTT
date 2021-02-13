89ab0ffbd9f75a54cc77bc5acadd7911e13b0ce6
99003678 <78944936+99003678@users.noreply.github.com>
2021-02-13 09:45:15 +0530

    inhand sal

---
+++ Implementation/src/inhand.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int inhand(int n,int bonus)
+{
+    float prf,avg;
+    int m;
+   prf=provfund(n);
+    m=n-bonus-2400-prof_tax()-incometax(n);
+    avg=m/12;
+    return avg;
+}
+ 
